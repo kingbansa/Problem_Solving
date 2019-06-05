cdb72ed69837527d269b174399823cc46ea3beaa
kingbansa <kingbansa@naver.com>
2019-06-05 14:37:53 +0900

    백준 3034번 앵그리 창영

--- 3034.cpp
+++ 3034.cpp
@@ -1,37 +1,18 @@
 #include <iostream>
-#include <queue>
-#include <vector>
+#include <cmath>
 using namespace std;
-bool v[505], visited[505][505];
 int main(void)
 {
-	int n, m, result = 0;
-	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
-		int a, b;
-		cin >> a >> b;
-		visited[a][b] = visited[b][a] = 1;
-		if (a == 1)
-			v[b] = true;
-		else if (b == 1)
-			v[a] = true;
+	int N, W, H; //성냥, 가로, 세로
+	cin >> N >> W >> H;
+	int degak = sqrt((W*W) + (H*H));
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		if (a <= H || a <= degak)
+			printf("DA\n");
+		else
+			printf("NE\n");
 	}
-
-	for (int i = 2; i <= n; i++) {
-		if (visited[1][i]) {
-			for (int j = 2; j <= n; j++) {
-				if (visited[i][j]) {
-					v[j] = true;
-				}
-			}
-		}
-	}
-
-	for (int i = 2; i <= n; i++) {
-		if (v[i])
-			result++;
-	}
-
-	printf("%d\n", result);
 	return 0;
 }
